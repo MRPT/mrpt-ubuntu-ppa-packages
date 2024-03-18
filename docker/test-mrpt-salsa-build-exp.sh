7c5c646215a4788452f05d8336826cc25754424f
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2024-03-18 22:41:01 +0100

    update docker

--- docker/test-mrpt-salsa-build-exp.sh
+++ docker/test-mrpt-salsa-build-exp.sh
@@ -4,7 +4,7 @@ set -x
 set -e
 
 cd  ~/code && \
-sudo docker run -v /home/jlblanco/code/mrpt-salsa:/tmp/mrpt-salsa -it --rm debian:mrpt \
+sudo docker run -v /home/jlblanco/code/mrpt-salsa:/tmp/mrpt-salsa -it --rm debian:mrpt.exp \
     /bin/bash -c "apt update && apt upgrade -y && apt dist-upgrade -y && cd /tmp && mkdir build && cp -r mrpt-salsa build/ && cd build/mrpt-salsa && rm .vscode/ -fr && gbp buildpackage"
 
 
